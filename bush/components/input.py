8617899ae73a267040dbf4d44cac9d36e7479cbf
= <jfrobinson767@gmail.com>
2022-11-07 21:13:23 -0600

    added some more components:

---
+++ bush/components/input.py
@@ -0,0 +1,37 @@
+"""
+input - basic input components
+"""
+from typing import Callable, Sequence, Union
+
+import pygame
+
+from bush.components import base_component
+
+
+class DumbButtonInput(base_component.Component):
+    """has a series of buttons with callbacks for each.  every frame the button is dow, is calls the callback w/ dt as a param"""
+
+    def __init__(
+        self,
+        id: Union[int, None] = None,
+        entities: Union[Sequence, None] = None,
+        key_to_callback_dict: dict[int:Callable] = {},
+    ):
+        super().__init__(id, entities, "input")
+        self._callbacks = key_to_callback_dict
+
+    def set_key(self, key: int, callback: Callable):
+        """Add or change a key event to look for."""
+        self._callbacks[key] = callback
+
+    def remove_key(self, key: int):
+        """Stop checking for a given key"""
+        del self._callbacks[key]
+
+    def update(self, dt: float):
+        """Check for keys and call callbacks"""
+        keys = pygame.key.get_pressed()
+        for key in self._callbacks:
+            if keys[key]:
+                self._callbacks[key](dt)
+                print("callback")
--- bush/components/render.py
+++ bush/components/render.py
@@ -2,3 +2,35 @@
 render
  - rendering components
 """
+from typing import Sequence, Union
+
+import pygame
+
+from bush.components import base_component
+
+
+class CircleRender(base_component.Component):
+    """
+    Render a circle as base image.
+    has radius and color attributes you can modify
+    if outline is true the circle will not be filled in
+    """
+
+    def __init__(
+        self,
+        id: Union[int, None] = None,
+        entities: Union[Sequence, None] = None,
+        color: Union[pygame.Color, Sequence[int]] = (255, 0, 0),
+        radius: int = 16,
+        outline: bool = False,
+    ):
+        super().__init__(id, entities, "render")
+        self.color = pygame.Color(*color)
+        self.radius = radius
+        self.outline = outline
+
+    def render(self, surface, offset: Sequence[float] = (0, 0)):
+        """Render my entities's circles to given surface, positions offset by given value"""
+        for entity in self.get_entities():
+            pos = entity.pos + offset
+            pygame.draw.circle(surface, self.color, pos, self.radius, self.outline)
