939d77c68225b29c2c3fe62bf8c0b4dcf9a4593f
= <jfrobinson767@gmail.com>
2022-11-07 21:14:08 -0600

    added test of APIS - player on grey background

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ Has Access to all other modules
 """
 import pygame
 
-from bush import util
+import player
+from bush import entity, util
 
 pygame.init()
 
@@ -18,20 +19,40 @@ class Game:
         self.fps = 60
         self.running = False
         self.bgcolor = (57, 57, 57)
+        ## game variables
+        self.player = player.Player(self.screen_size / 2, 0)
+        self.entity_group = entity.EntityGroup(self.player)
 
     def run(self):
         self.screen = pygame.display.set_mode(util.rvec(self.screen_size))
         self.running = True
         dt = 0
         while self.running:
+            # get events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.quit()
 
+            # clear screen
             self.screen.fill(self.bgcolor)
+            # update input, physics
+            self.entity_group.update(dt, ("input", "physics"))
+            # render
+            self.entity_group.render(self.screen)
+            # update ai and state
+            self.entity_group.update(dt, ("state_machine", "ai"))
+            # update screen
             pygame.display.update()
-
+            # tick and get delta time
             dt = self.clock.tick(self.fps)
+
+        # after the game loop
+        # reset things
+        pygame.quit()
+        self.screen = None
+
+    def quit(self):
+        self.running = False
 
 
 Game().run()
---
+++ player.py
@@ -0,0 +1,44 @@
+"""
+player - class for the player
+"""
+from typing import Union
+
+import pygame
+
+from bush import components, entity
+
+
+class Player(entity.Entity):
+    """main player of the game
+
+    Args:
+        pos: initial position of the player (list, pygame.Vector2, etc.)
+        id: player's integer id
+    """
+
+    def __init__(self, pos: Union[pygame.Vector2, list, tuple], id=None):
+        speed = 96
+        input_component = components.DumbButtonInput(
+            id=0,
+            entities=(self,),
+            key_to_callback_dict={
+                pygame.K_UP: lambda dt: self.move(
+                    pygame.Vector2(0, -speed * dt / 1000)
+                ),
+                pygame.K_DOWN: lambda dt: self.move(
+                    pygame.Vector2(0, speed * dt / 1000)
+                ),
+                pygame.K_LEFT: lambda dt: self.move(
+                    pygame.Vector2(-speed * dt / 1000, 0)
+                ),
+                pygame.K_RIGHT: lambda dt: self.move(
+                    pygame.Vector2(speed * dt / 1000, 0)
+                ),
+            },
+        )
+        render_component = components.CircleRender(1, (self,))
+        super().__init__(pos, id, input_component, render_component)
+
+    def move(self, vec: pygame.Vector2):
+        """Move the player by given vec"""
+        self.pos += vec
