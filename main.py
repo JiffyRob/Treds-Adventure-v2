37d01467b837a1d2a3de555c5d05525dad3bf567
= <jfrobinson767@gmail.com>
2022-11-26 20:20:10 -0600

    fixed main and player to reflect new API.  Test successful

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Has Access to all other modules
 import pygame
 
 import player
-from bush import entity, util
+from bush import entity, util, level, color
 
 pygame.init()
 
@@ -18,49 +18,40 @@ class Game:
         self.clock = pygame.time.Clock()
         self.fps = 500
         self.running = False
-        self.bgcolor = (57, 57, 57)
+        self.bgcolor = color.GREY
         ## game variables
-        self.player = player.Player(self.screen_size / 2, 0)
-        self.entity_group = entity.EntityGroup(self.player)
+        self.player = player.Player(self.screen_size / 2, self.screen_size * 2)
+        self.entity_group = level.TopDownGroup(
+            cam_size=self.screen_size,
+            map_size=self.screen_size * 2,
+            pos=(0, 0),
+            follow=self.player,
+        )
+        self.entity_group.add(self.player)
 
     def run(self):
         self.screen = pygame.display.set_mode(util.rvec(self.screen_size))
         self.running = True
         dt = 0
-        min_fps = 2000  # pygame limit
         # initial screen setup
         self.screen.fill(self.bgcolor)
-        self.entity_group.render(self.screen)
-        pygame.display.flip()
-        while self.running:
-            # get events
+        running = True
+        while True:
+            if not running:
+                break
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.quit()
-
-            # clear screen
+                    running = False
             self.screen.fill(self.bgcolor)
-            # update input, physics
-            self.entity_group.update(dt, ("input", "physics"))
-            # render
-            rects = self.entity_group.render(self.screen)
-            # update ai and state
-            self.entity_group.update(dt, ("state_machine", "ai"))
-            # update screen
-            pygame.display.update(rects)
-            # tick and get delta time
-            dt = self.clock.tick(self.fps)
-            # keep fps data
-            fps = self.clock.get_fps()
-            if fps != 0:
-                min_fps = min(min_fps, fps)
+            self.entity_group.update(dt / 1000)
+            self.entity_group.draw(self.screen)
+            pygame.display.flip()
+            dt = self.clock.tick(60)
 
         # after the game loop
         # reset things
         pygame.quit()
         self.screen = None
-        # print minimum fps
-        print(f"Game terminated successfully (min fps {min_fps})")
 
     def quit(self):
         self.running = False
--- player.py
+++ player.py
@@ -5,7 +5,7 @@ from typing import Union
 
 import pygame
 
-from bush import component, entity
+from bush import entity, util, color
 
 
 class Player(entity.Entity):
@@ -16,30 +16,27 @@ class Player(entity.Entity):
         id: player's integer id
     """
 
-    def __init__(self, pos: Union[pygame.Vector2, list, tuple], id=None):
-        speed = 96
-        input_component = component.DumbButtonInput(
-            id=0,
-            entities=(self,),
-            key_to_callback_dict={
-                pygame.K_UP: lambda dt: self.move(
-                    pygame.Vector2(0, -speed * dt / 1000)
-                ),
-                pygame.K_DOWN: lambda dt: self.move(
-                    pygame.Vector2(0, speed * dt / 1000)
-                ),
-                pygame.K_LEFT: lambda dt: self.move(
-                    pygame.Vector2(-speed * dt / 1000, 0)
-                ),
-                pygame.K_RIGHT: lambda dt: self.move(
-                    pygame.Vector2(speed * dt / 1000, 0)
-                ),
-            },
-        )
-        render_component = component.CircleRender(1, (self,))
-        super().__init__(pos, id, input_component, render_component)
+    def __init__(self, pos: Union[pygame.Vector2, list, tuple], map_size):
+        super().__init__(util.circle_surf(6, color.RED, 1), pos, ())
+        self.speed = 64
+        self.map_rect = pygame.Rect(0, 0, *map_size)
 
-    def move(self, vec: pygame.Vector2):
-        """Move the player by given vec"""
-        self.pos += vec
-        self.dirty = 1
+    def input(self, dt):
+        keys = pygame.key.get_pressed()
+        self.velocity = pygame.Vector2()
+        if keys[pygame.K_LEFT]:
+            self.velocity.x -= 1
+        if keys[pygame.K_RIGHT]:
+            self.velocity.x += 1
+        if keys[pygame.K_UP]:
+            self.velocity.y -= 1
+        if keys[pygame.K_DOWN]:
+            self.velocity.y += 1
+        if self.velocity:
+            self.velocity.scale_to_length(self.speed)
+
+    def limit(self, dt):
+        print(self.rect)
+        self.rect.clamp_ip(self.map_rect)
+        self.pos = self.rect.center
+        print(self.rect)
