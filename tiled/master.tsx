fffe62d8ae136fdf3aff36834fba9edd0d573dac
= <jfrobinson767@gmail.com>
2022-12-10 21:49:54 -0600

    added tiled directory, started working on master tileset

---
+++ tiled/master.tsx
@@ -0,0 +1,236 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="master" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="../resources/tiles/master.png" width="1028" height="1028"/>
+ <tile id="2112">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" x="16" y="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2113">
+  <objectgroup draworder="index" id="2">
+   <object id="2" template="collision_templates/stumptopleft.tx" x="0" y="16" rotation="90"/>
+  </objectgroup>
+ </tile>
+ <tile id="2114">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" x="16" y="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2115">
+  <objectgroup draworder="index" id="2">
+   <object id="4" template="collision_templates/stumptopleft.tx" gid="0" x="0" y="16" rotation="0">
+    <polygon points="0,0 0,-13.9565 10.3913,-14.0435 14.1304,-10.087 14.2609,0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2116">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" x="16" y="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2117">
+  <objectgroup draworder="index" id="2">
+   <object id="4" template="collision_templates/stumptopleft.tx" gid="0" x="0" y="16" rotation="0">
+    <polygon points="0,0 0,-13.9565 10.3913,-14.0435 14.1304,-10.087 14.2609,0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2118">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="16">
+    <polygon points="0,0 0,-13.9565 -8.29641,-14.0435 -11.2817,-10.087 -11.3859,0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2119">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0109" y="16.0625">
+    <polygon points="0,0 0,-13.9565 8.29641,-14.0435 11.2817,-10.087 11.3859,0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2120">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" x="16" y="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2121">
+  <objectgroup draworder="index" id="2">
+   <object id="4" template="collision_templates/stumptopleft.tx" gid="0" x="0" y="16" rotation="0">
+    <polygon points="0,0 0,-13.9565 10.3913,-14.0435 14.1304,-10.087 14.2609,0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2122">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" x="16" y="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2123">
+  <objectgroup draworder="index" id="2">
+   <object id="2" template="collision_templates/stumptopleft.tx" gid="0" x="0" y="16" rotation="0">
+    <polygon points="0,0 0,-13.9565 10.3913,-14.0435 14.1304,-10.087 14.2609,0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2124">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" x="16" y="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2125">
+  <objectgroup draworder="index" id="2">
+   <object id="2" template="collision_templates/stumptopleft.tx" gid="0" x="0" y="16" rotation="0">
+    <polygon points="0,0 0,-13.9565 10.3913,-14.0435 14.1304,-10.087 14.2609,0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2126">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" x="16" y="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2127">
+  <objectgroup draworder="index" id="2">
+   <object id="2" template="collision_templates/stumptopleft.tx" gid="0" x="0" y="16" rotation="0">
+    <polygon points="0,0 0,-13.9565 10.3913,-14.0435 14.1304,-10.087 14.2609,0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2176">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="16" y="0" rotation="0">
+    <polygon points="0,0 0,13.9565 -10.3913,14.0435 -14.1304,10.087 -14.2609,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2177">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="0" y="0" rotation="0">
+    <polygon points="0,0 0,13.9565 10.3913,14.0435 14.1304,10.087 14.2609,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2178">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="16" y="0" rotation="0">
+    <polygon points="0,0 0,13.9565 -10.3913,14.0435 -14.1304,10.087 -14.2609,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2179">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="0" y="0" rotation="0">
+    <polygon points="0,0 0,13.9565 10.3913,14.0435 14.1304,10.087 14.2609,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2180">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="16" y="0" rotation="0">
+    <polygon points="0,0 0,13.9565 -10.3913,14.0435 -14.1304,10.087 -14.2609,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2181">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="0" y="0" rotation="0">
+    <polygon points="0,0 0,13.9565 10.3913,14.0435 14.1304,10.087 14.2609,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2182">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="16" y="0">
+    <polygon points="0,0 0,16 -3,16.0625 -4,14.0625 -4.1875,8.125"/>
+   </object>
+   <object id="3" x="4" y="0.1875">
+    <polygon points="0,0 -2.9375,7.75 -3.125,11.6875 1.0625,11.875 5.0625,9.6875 7.6875,7.75 12,-0.1875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2183">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="0" y="0">
+    <polygon points="0,0 0,16 2.0625,16 15.125,12 14,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2184">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="16" y="0" rotation="0">
+    <polygon points="0,0 0,13.9565 -10.3913,14.0435 -14.1304,10.087 -14.2609,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2185">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="0" y="0" rotation="0">
+    <polygon points="0,0 0,13.9565 10.3913,14.0435 14.1304,10.087 14.2609,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2186">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="16" y="0" rotation="0">
+    <polygon points="0,0 0,13.9565 -10.3913,14.0435 -14.1304,10.087 -14.2609,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2187">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="0" y="0" rotation="0">
+    <polygon points="0,0 0,13.9565 10.3913,14.0435 14.1304,10.087 14.2609,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2188">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="16" y="0" rotation="0">
+    <polygon points="0,0 0,13.9565 -10.3913,14.0435 -14.1304,10.087 -14.2609,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2189">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="0" y="0" rotation="0">
+    <polygon points="0,0 0,13.9565 10.3913,14.0435 14.1304,10.087 14.2609,-0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2190">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" gid="0" x="14.9375" y="14.9375" rotation="0">
+    <polygon points="0,0 0,-13.9565 -10.3913,-14.0435 -14.1304,-10.087 -14.2609,0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2191">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="collision_templates/stumptopleft.tx" x="14.5625" y="13.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="2432">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0">
+    <polygon points="0,0 0.0625,14 -3.0625,15.9375 -7,15.9375 -8.875,8.375 -5,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="Ground" type="mixed" tile="-1">
+   <wangcolor name="Grass" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Dark Grass" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="Sand" color="#0000ff" tile="-1" probability="1"/>
+   <wangcolor name="Pink" color="#ff7700" tile="-1" probability="1"/>
+   <wangcolor name="Snow" color="#00e9ff" tile="-1" probability="1"/>
+   <wangcolor name="Evil Dirt" color="#ff00d8" tile="-1" probability="1"/>
+  </wangset>
+  <wangset name="Misc" type="mixed" tile="-1">
+   <wangcolor name="CloudsBluesky" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="CloudsOrangeSky" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="CloudsNoSky" color="#0000ff" tile="-1" probability="1"/>
+   <wangcolor name="Hole" color="#ff7700" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>
