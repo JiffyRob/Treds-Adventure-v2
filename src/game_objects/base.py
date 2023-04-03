9ad401ff9b0ff447fc26c599dc1817cb09843bcb
John Robinson <jfrobinson767@gmail.com>
2023-04-02 19:59:14 -0500

    forgot to commit the aforementioned package...

---
+++ src/game_objects/base.py
@@ -0,0 +1,292 @@
+import pygame
+
+import environment
+import scripts
+from bush import animation, asset_handler, entity, physics, sound_manager, timer, util
+
+player = sound_manager.glob_player
+loader = asset_handler.AssetHandler("resources/sprites")
+
+
+class StaticGameObject(entity.Actor):
+    def __init__(
+        self,
+        pos,
+        surface,
+        engine,
+        groups=(),
+        topleft=False,
+        anim_dict=None,
+        id=None,
+        layer=None,
+        script=None,
+        interaction_script=None,
+        event_group=None,
+    ):
+        super().__init__(pos, surface, groups, id, layer, topleft)
+        # scripting
+        self.engine = engine
+        self.script = scripts.get_script(script, self, engine, event_group)
+        if self.script is not None:
+            self.script.begin()
+        self.interaction_script = scripts.get_script(
+            interaction_script, self, engine, event_group
+        )
+        # state
+        self.facing = "down"
+        self.state = "idle"
+        self.pushed_state = None
+        self.anim_dict = anim_dict
+        self.interacting = False
+
+    # scripting
+    def update_script(self, dt):
+        if self.script is not None:
+            self.script.update(dt)
+        if self.interaction_script is not None:
+            self.interaction_script.update(dt)
+            if self.interaction_script.complete:
+                self.script.unpause()
+                self.interaction_script.reset()
+                self.interacting = False
+
+    def interact(self):
+        if self.interaction_script is not None:
+            self.interaction_script.begin()
+            self.interacting = True
+            if self.script is not None:
+                self.script.pause()
+
+    # scripting commands
+    def push_state(self, state):
+        self.pushed_state = state
+
+    def unpush_state(self):
+        self.pushed_state = None
+
+    @staticmethod
+    def play_sound(name):
+        player.play(name)
+
+    @staticmethod
+    def get_sound_player():
+        return player
+
+    def spawn_particle(self, name, pos):
+        # TODO
+        print("spawning a particle", name, pos)
+
+    def unload_script(self):
+        self.script.end()
+        self.script = None
+
+    # state
+    def update_state(self):
+        if self.pushed_state:
+            self.state = self.pushed_state
+        else:
+            self.state = None
+
+    # rendering
+    def update_image(self):
+        if self.anim_dict:
+            self.anim = self.anim_dict.get(
+                f"{self.state} {self.facing}",
+                self.anim_dict.get(
+                    f"{self.state} {util.round_string_direction(self.facing)}",
+                    self.anim_dict.get(self.state, None),
+                ),
+            )
+        if self.anim is not None:
+            self.image = self.anim.image()
+
+    # engine
+    def update(self, dt):
+        # update script
+        self.update_script(dt)
+        # update state
+        self.update_state()
+        # update image
+        self.update_image()
+
+
+class DynamicGameObject(StaticGameObject):
+    def __init__(
+        self,
+        pos,
+        surface,
+        engine,
+        physics_data,
+        weight=10,
+        speed=48,
+        groups=(),
+        map_env=None,
+        topleft=False,
+        anim_dict=None,
+        id=None,
+        layer=None,
+        start_health=12,
+        max_health=12,
+        on_death=None,
+        script=None,
+        interaction_script=None,
+        event_group=None,
+    ):
+        super().__init__(
+            pos,
+            surface,
+            engine,
+            groups,
+            topleft,
+            anim_dict,
+            id,
+            layer,
+            script,
+            interaction_script,
+            event_group,
+        )
+        # motion
+        self.desired_velocity = pygame.Vector2()
+        self.desired_position = None
+        self.weight = weight
+        self.speed = speed
+        self.environment = map_env
+        if self.environment is not None:
+            self.current_terrain = self.environment.environment_data("default")
+        else:
+            self.current_terrain = environment.EnvironmentData(
+                **environment.DEFAULT_DATA
+            )
+        self.current_terrain_name = "default"
+        self.physics_data = physics_data
+        self.collision_rect = self.rect
+        self.mask = pygame.Mask(self.rect.size, fill=True)
+        # health
+        self.current_health = start_health
+        self.health_capacity = max_health
+        self.on_death = on_death
+        if self.on_death is None:
+            self.on_death = lambda: None
+        self.poison_hurt_timer = timer.Timer(0)
+        self.poison_stop_timer = timer.Timer(0)
+
+    # scripting commands
+    def interact(self):
+        self.face(self.engine.player.pos)
+        super().interact()
+
+    def move(self, direction):
+        self.desired_velocity = direction
+
+    def move_toward(self, dest):
+        if (self.pos - dest).length_squared() > 0.5:
+            self.desired_velocity = (dest - self.pos).scale_to_length(self.speed)
+
+    def move_to(self, dest):
+        self.desired_position = dest
+
+    def stop(self, force=False):
+        self.desired_velocity *= 0
+        # here are two fun ways to avoid branching!
+        self.velocity *= not force
+        self.desired_position = (None, self.desired_position)[force]
+
+    def face(self, pos):
+        self.stop(True)
+        self.facing = util.round_string_direction(util.string_direction(pos - self.pos))
+
+    def terrain_allows_move(self, move):
+        return move in self.current_terrain.moves
+
+    def update_terrain(self):
+        if self.environment is None:
+            return
+        self.current_terrain_name = self.environment.get_environment_at(
+            self.mask, self.rect.topleft
+        )
+        self.current_terrain = self.environment.environment_data(
+            self.current_terrain_name
+        )
+
+    def update_rects(self):
+        self.rect.center = self.pos
+        self.collision_rect.center = self.pos
+
+    def change_environment(self, new_env):
+        self.environment = new_env
+
+    def heal(self, amount):
+        self.current_health = min(self.current_health + amount, self.health_capacity)
+
+    def hurt(self, amount):
+        self.current_health = max(self.current_health - amount, 0)
+
+    def poison(self, amount=1, length=20000, interval=3500):
+        self.poison_hurt_timer = timer.Timer(interval, lambda: self.hurt(amount), True)
+        self.poison_stop_timer = timer.Timer(length, self.end_poison)
+
+    def end_poison(self):
+        self.poison_hurt_timer = timer.Timer(0)
+        self.poison_stop_timer = timer.Timer(0)
+
+    def update_health(self):
+        # take care of poison
+        self.poison_hurt_timer.update()
+        self.poison_stop_timer.update()
+        # reclamp health in case poison screwed it up
+        self.current_health = pygame.math.clamp(
+            self.current_health, 0, self.health_capacity
+        )
+        # DIE
+        if self.current_health == 0:
+            self.on_death()
+
+    def update_state(self):
+        if self.velocity:
+            self.state = self.current_terrain.move_state
+            self.facing = util.string_direction(self.velocity)
+        else:
+            self.state = self.current_terrain.idle_state
+
+    def update(self, dt):
+        # calculate and update self.current_terrain
+        self.update_terrain()
+        # go to desired position
+        if self.desired_position is not None:
+            self.move_toward(self.desired_position)
+        # modify speed
+        if self.desired_velocity:
+            self.desired_velocity.scale_to_length(
+                self.speed * self.current_terrain.speed
+            )
+        # slippety-slide!
+        if self.current_terrain.traction != 1:
+            sliding = True
+            self.velocity += self.desired_velocity * self.current_terrain.traction
+        else:
+            sliding = False
+            self.velocity = self.desired_velocity
+        # physics update
+        self.update_rects()
+        physics.dynamic_update(self, dt, sliding)
+        self.update_rects()
+        # update health
+        self.update_health()
+        # update state
+        super().update(dt)
+
+
+def get_anim_dict(path, size):
+    frames = loader.load(
+        path + ".png", loader=asset_handler.load_spritesheet, frame_size=size
+    )
+    return {
+        "walk down": animation.Animation(frames[0:16:4], 150),
+        "walk up": animation.Animation(frames[1:17:4], 150),
+        "walk left": animation.Animation(frames[2:18:4], 150),
+        "walk right": animation.Animation(frames[3:18:4], 150),
+        "idle down": animation.Animation(frames[0:1]),
+        "idle up": animation.Animation(frames[1:2]),
+        "idle left": animation.Animation(frames[2:3]),
+        "idle right": animation.Animation(frames[3:4]),
+    }
---
+++ src/game_objects/player.py
@@ -0,0 +1,249 @@
+"""
+player - class for the player
+"""
+import pygame
+
+SPEED_MEANDERING = 32
+SPEED_WALKING = 96
+SPEED_RUNNING = 128
+
+from bush import animation, asset_handler, event_binding, physics, util
+from game_objects import base
+
+loader = asset_handler.AssetHandler("resources/sprites/player")
+
+
+class Player(base.DynamicGameObject):
+    """main player of the game
+
+    Args:
+        pos: initial position of the player (list, pygame.Vector2, etc.)
+        id: player's integer id
+    """
+
+    def __init__(self, pos, layer, map_env, engine, interactable_group, **__):
+        tiny_frames = loader.load(
+            "tiny.png", loader=asset_handler.load_spritesheet, frame_size=(16, 16)
+        )
+        foot_frames = loader.load(
+            "feet-default.png",
+            loader=asset_handler.load_spritesheet,
+            frame_size=(16, 32),
+        )
+        torso_frames = loader.load(
+            "torso-default.png",
+            loader=asset_handler.load_spritesheet,
+            frame_size=(16, 32),
+        )
+        arm_frames = loader.load(
+            "arms-default.png",
+            loader=asset_handler.load_spritesheet,
+            frame_size=(16, 32),
+        )
+        head_frames = loader.load(
+            "head-default.png",
+            loader=asset_handler.load_spritesheet,
+            frame_size=(16, 32),
+        )
+        anim_dict = {
+            "tiny walk down": animation.Animation(tiny_frames[0:16:4], 150),
+            "tiny walk up": animation.Animation(tiny_frames[1:17:4], 150),
+            "tiny walk left": animation.Animation(tiny_frames[2:18:4], 150),
+            "tiny walk right": animation.Animation(tiny_frames[3:18:4], 150),
+            "tiny idle down": animation.Animation(tiny_frames[0:1]),
+            "tiny idle up": animation.Animation(tiny_frames[1:2]),
+            "tiny idle left": animation.Animation(tiny_frames[2:3]),
+            "tiny idle right": animation.Animation(tiny_frames[3:4]),
+            "walk down": animation.MultiAnimation(
+                (
+                    animation.Animation(foot_frames[0:8], 70),
+                    animation.Animation(torso_frames[0:8], 70),
+                    animation.Animation(arm_frames[0:8], 70),
+                    animation.Animation(head_frames[0:8], 70),
+                )
+            ),
+            "walk up": animation.MultiAnimation(
+                (
+                    animation.Animation(foot_frames[8:16], 70),
+                    animation.Animation(torso_frames[8:16], 70),
+                    animation.Animation(arm_frames[8:16], 70),
+                    animation.Animation(head_frames[8:16], 70),
+                )
+            ),
+            "walk right": animation.MultiAnimation(
+                (
+                    animation.Animation(foot_frames[16:24], 70),
+                    animation.Animation(torso_frames[16:24], 70),
+                    animation.Animation(arm_frames[16:24], 70),
+                    animation.Animation(head_frames[16:24], 70),
+                )
+            ),
+            "walk left": animation.MultiAnimation(
+                (
+                    animation.Animation(foot_frames[16:24], 70, mirror_x=True),
+                    animation.Animation(torso_frames[16:24], 70, mirror_x=True),
+                    animation.Animation(arm_frames[16:24], 70, mirror_x=True),
+                    animation.Animation(head_frames[16:24], 70, mirror_x=True),
+                )
+            ),
+            "idle down": animation.MultiAnimation(
+                (
+                    animation.Animation(foot_frames[7:8], 70),
+                    animation.Animation(torso_frames[7:8], 70),
+                    animation.Animation(arm_frames[7:8], 70),
+                    animation.Animation(head_frames[7:8], 70),
+                )
+            ),
+            "idle up": animation.MultiAnimation(
+                (
+                    animation.Animation(foot_frames[15:16], 70),
+                    animation.Animation(torso_frames[15:16], 70),
+                    animation.Animation(arm_frames[15:16], 70),
+                    animation.Animation(head_frames[15:16], 70),
+                )
+            ),
+            "idle right": animation.MultiAnimation(
+                (
+                    animation.Animation(foot_frames[23:24], 70),
+                    animation.Animation(torso_frames[23:24], 70),
+                    animation.Animation(arm_frames[23:24], 70),
+                    animation.Animation(head_frames[23:24], 70),
+                )
+            ),
+            "idle left": animation.MultiAnimation(
+                (
+                    animation.Animation(foot_frames[23:24], 70, mirror_x=True),
+                    animation.Animation(torso_frames[23:24], 70, mirror_x=True),
+                    animation.Animation(arm_frames[23:24], 70, mirror_x=True),
+                    animation.Animation(head_frames[23:24], 70, mirror_x=True),
+                )
+            ),
+        }
+        super().__init__(
+            pos,
+            pygame.Surface((16, 16)),
+            engine,
+            map_env=map_env,
+            physics_data=physics.PhysicsData(
+                physics.TYPE_DYNAMIC, pygame.sprite.Group()
+            ),
+            speed=72,
+            anim_dict=anim_dict,
+            id="player",
+            layer=layer,
+            start_health=6,
+            max_health=12,
+        )
+        self.collision_rect = self.rect
+        self.engine = engine
+        self.speeds = {
+            "x": SPEED_WALKING,
+            "y": SPEED_WALKING,
+        }
+        self.current_mana = 6
+        self.mana_capacity = 12
+        self.items = []
+        self.save_state = self.engine.state
+        self.save_fields = (
+            "current_mana",
+            "mana_capacity",
+            "current_health",
+            "health_capacity",
+            "items",
+        )
+        self.tiny = False
+        self.interactable_group = interactable_group
+        self.interactor = None
+        self.input_locked = False
+        self.load_data()
+
+    def save_data(self):
+        for field in self.save_fields:
+            self.save_state.set(field, getattr(self, field), "player")
+
+    def load_data(self):
+        for field in self.save_fields:
+            setattr(self, field, self.save_state.get(field, "player"))
+
+    def event(self, event):
+        if event.type == event_binding.BOUND_EVENT and not self.input_locked:
+            self.command(event.name)
+
+    def command(self, command_name):
+        words = command_name.split(" ")
+        if words[0] == "interact":
+            self.interact()
+        if words[0] == "start":
+            if words[1] == "meandering":
+                self.speeds[words[2]] = SPEED_MEANDERING
+            if words[1] == "walking":
+                self.speeds[words[2]] = SPEED_WALKING
+            if words[1] == "running":
+                self.speeds[words[2]] = SPEED_RUNNING
+            self.speed = max(self.speeds.values())
+        if words[0] == "go":
+            directions = {
+                "up": (self.desired_velocity.x, -self.speed),
+                "down": (self.desired_velocity.x, self.speed),
+                "left": (-self.speed, self.desired_velocity.y),
+                "right": (self.speed, self.desired_velocity.y),
+            }
+            self.desired_velocity = pygame.Vector2(directions[words[1]])
+        if words[0] == "stop":
+            directions = {
+                "up": (self.desired_velocity.x, 0),
+                "down": (self.desired_velocity.x, 0),
+                "left": (0, self.desired_velocity.y),
+                "right": (0, self.desired_velocity.y),
+            }
+            self.desired_velocity = pygame.Vector2(directions[words[1]])
+
+        if self.desired_velocity:
+            self.desired_velocity.scale_to_length(self.speed)
+
+    def get_interaction_rect(self):
+        interaction_rect = self.rect.copy()
+        interaction_rect.center += (
+            util.string_direction_to_vec(util.round_string_direction(self.facing)) * 16
+        )
+        return interaction_rect
+
+    def interact(self):
+        for sprite in self.interactable_group.sprites():
+            if sprite.rect.colliderect(self.get_interaction_rect()):
+                sprite.interact()
+                print("interacting with sprite", sprite.get_id())
+                self.interactor = sprite
+                self.stop(True)
+                break
+
+    def change_collision_group(self, collision_group):
+        self.physics_data = physics.PhysicsData(physics.TYPE_DYNAMIC, collision_group)
+
+    def change_layer(self, layer):
+        self._layer = layer
+
+    def update_rects(self):
+        self.rect = self.image.get_rect(center=self.pos)
+        self.collision_rect = pygame.Rect(0, 0, 10, 10)
+        self.collision_rect.midbottom = self.rect.midbottom
+        self.mask = pygame.Mask(self.rect.size, False)
+        self.mask.draw(
+            pygame.Mask(self.collision_rect.size, True),
+            self.collision_rect.topleft - pygame.Vector2(self.rect.topleft),
+        )
+
+    def update_state(self):
+        super().update_state()
+        if self.tiny:
+            self.state = "tiny " + self.state
+        if self.interactor is not None:
+            if self.interactor.interacting:
+                self.input_locked = True
+            else:
+                self.input_locked = False
+                self.interactor = None
+
+    def update(self, dt):
+        self.current_mana = min(self.current_mana + (dt * 0.1), self.mana_capacity)
+        super().update(dt)
